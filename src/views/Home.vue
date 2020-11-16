f7829d961c5aee086eb7ce1cb31a07bc6552eb07
gubiarpa <billy.arredondo@pucp.pe>
2020-11-16 01:34:57 -0500

    Checkboxes Added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,56 @@
 <template>
-  <div class="home">
-  </div>
+  <form>
+    <input
+      type="text"
+      class="form-control my-2"
+      placeholder="Ingrese nombre"
+      v-model="tarea.nombre"
+      >
+      <!-- Javascript -->
+      <div class="form-check form-check-inline">
+        <input
+          id="chk-1"
+          value="js"
+          v-model="tarea.categorias"
+          type="checkbox"
+          class="form-check-input">
+        <label
+          for="chk-1"
+          class="form-check-label">
+            Javascript
+        </label>
+      </div>
+      <!-- Node.js -->
+      <div class="form-check form-check-inline">
+        <input
+          id="chk-1"
+          v-model="tarea.categorias"
+          value="nd"
+          type="checkbox"
+          class="form-check-input">
+        <label
+          for="chk-1"
+          class="form-check-label">
+            Node.js
+        </label>
+      </div>
+      <hr>
+      {{tarea}}
+  </form>
 </template>
 
 <script>
 export default {
   name: 'Home',
   components: {
-  }
+  },
+  data() {
+    return {
+      tarea: {
+        nombre: [],
+        categorias: []
+      }
+    }
+  },
 }
 </script>
